01dc66a944999a8dbd19aeac6483b3083d6487a7
Sudeepa-R <119294212+Sudeepa-R@users.noreply.github.com>
2023-12-16 13:55:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,9 @@ const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
+       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          <span style={{ color: "#FC1503" }}>Play Loop</span>
+        </Typography>
     </Link>
     <SearchBar />
   </Stack>
