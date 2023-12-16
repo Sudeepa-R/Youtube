038d950d0be19f7329fb2f20c897bdae12fd823a
Sudeepa-R <119294212+Sudeepa-R@users.noreply.github.com>
2023-12-16 14:35:05 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,9 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <div style={{display:'flex', gap:'10px'}}>
+      <div className="logo-name" style={{display:'flex', gap:'10px'}}>
       <img src={logo} alt="logo" height={45} />
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+      <Typography className="logo-name" variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
           <span >Play Loop</span>
         </Typography>
       </div>
